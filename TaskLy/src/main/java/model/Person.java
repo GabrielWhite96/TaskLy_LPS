aeb10082ef9c6683da226d353eea0ddca29089a2
Wekisley Souza <wekisleysouza.a@gmail.com>
2024-11-08 18:57:00 -0300

    Atributos adicionados na Person.

--- TaskLy/src/main/java/model/Person.java
+++ TaskLy/src/main/java/model/Person.java
@@ -4,10 +4,24 @@
  */
 package model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 /**
  *
  * @author wekisley
  */
 public class Person {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @OneToOne(mappedBy="person")
+    private Login login;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String jobTitle;
+    private boolean gender;
 }
