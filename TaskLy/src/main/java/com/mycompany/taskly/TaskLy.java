59f88ffaaf04981a80c06ed24f801564b93c0dff
Gabriel Fernandes <gabrielfvp32015@gmail.com>
2024-11-09 14:06:35 -0300

    update

---
+++ TaskLy/src/main/java/com/mycompany/taskly/TaskLy.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taskly;
+
+import controller.LoginController;
+import controller.PersonController;
+import dao.ConnectionDB;
+import dao.LoginDAO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import java.util.ArrayList;
+import java.util.List;
+import model.Login;
+import view.CreatePerson;
+import view.LoginView;
+import view.ProjectsMenu;
+
+/**
+ *
+ * @author wekisley
+ */
+public class TaskLy {
+    public static void main(String[] args) throws Exception {
+        EntityManagerFactory factory = ConnectionDB.getFactory();
+        
+//        CreatePerson person = new CreatePerson();
+//        person.setVisible(true);
+//        
+        ProjectsMenu menu = new ProjectsMenu();
+        menu.setVisible(true);
+        
+//        LoginController loginController = new LoginController();
+//        Login login = loginController.createLogin("aaa", "1234567");
+//        PersonController personController = new PersonController();
+//        personController.createNewUser("Wekisley", login, "", "21212", "este", "M");
+//        int id = loginController.getUserAccount("wekisleysouza.a@gmail.com", "kkjkjjj");
+//        System.out.println("Id: " + id);
+//        factory.close();
+    }
+}
