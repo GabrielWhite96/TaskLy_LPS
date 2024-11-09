dd78222d1aa58d33415b49e191aa712cd0ec71a0
Wekisley Souza <wekisleysouza.a@gmail.com>
2024-11-09 17:31:09 -0300

    Modelo de cards no net beans na TesteTable.

--- TaskLy/src/main/java/com/mycompany/taskly/TaskLy.java
+++ TaskLy/src/main/java/com/mycompany/taskly/TaskLy.java
@@ -16,6 +16,7 @@ import model.Login;
 import view.CreatePerson;
 import view.CreateProject;
 import view.LoginView;
+import view.TesteTable;
 
 /**
  *
@@ -25,15 +26,17 @@ public class TaskLy {
     public static void main(String[] args) throws Exception {
         EntityManagerFactory factory = ConnectionDB.getFactory();
         
-        CreateProject project = new CreateProject();
-        project.setVisible(true);
+//        CreateProject project = new CreateProject();
+//        project.setVisible(true);
+        TesteTable table = new TesteTable();
+        table.setVisible(true);
 
 //        LoginController loginController = new LoginController();
-//        Login login = loginController.createLogin("aaa", "1234567");
+//        Login login = loginController.createLogin("aaqaaaqqa", "123s4567");
 //        PersonController personController = new PersonController();
-//        personController.createNewUser("Wekisley", login, "", "21212", "este", "M");
+//        personController.createNewUser("Wekisqqqley", login, "", "21q212", "esqte", "M");
 //        int id = loginController.getUserAccount("wekisleysouza.a@gmail.com", "kkjkjjj");
 //        System.out.println("Id: " + id);
-//        factory.close();
+        factory.close();
     }
 }
