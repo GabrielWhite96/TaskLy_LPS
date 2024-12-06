185e8f747048118dbb485c8712ca5388c049c5ac
Wekisley Souza <wekisleysouza.a@gmail.com>
2024-12-06 15:39:45 -0300

    Criação de tarefas implementada

--- TaskLy/src/main/java/view/ProjectView.java
+++ TaskLy/src/main/java/view/ProjectView.java
@@ -5,6 +5,7 @@
 package view;
 
 import model.Project;
+import utils.MenuNavigation;
 
 /**
  *
@@ -365,14 +366,9 @@ public class ProjectView extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    
-    private void goToProjectsMenu(){
-        
-    }
     
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        MenuNavigation.goToProjectsMenu(this);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -388,7 +384,9 @@ public class ProjectView extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        CreateTask taskCreationScreen = new CreateTask(this.project);
+        taskCreationScreen.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
