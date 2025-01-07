4f6c63375193a83e2ffe4f2bd59c0e30258747a6
Gabriel Fernandes <gabrielfvp32015@gmail.com>
2025-01-06 21:13:24 -0300

    correção menu navegação

--- TaskLy/src/main/java/view/CreatePerson.java
+++ TaskLy/src/main/java/view/CreatePerson.java
@@ -578,7 +578,7 @@ public class CreatePerson extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        MenuNavigation.goToClockIn(this);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
--- TaskLy/src/main/java/view/CreateTask.java
+++ TaskLy/src/main/java/view/CreateTask.java
@@ -548,7 +548,7 @@ public class CreateTask extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        MenuNavigation.goToClockIn(this);
     }//GEN-LAST:event_jButton1ActionPerformed
     
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
